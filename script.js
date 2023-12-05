d6cd81efafb0b6b4380e40dac6d5b1cd84b4ef53
Phuong Nguyen <tronganhnguyen222@gmail.com>
2023-12-05 11:54:35 +0700

    [UPDATE][FE] build JS advertisement

---
+++ script.js
@@ -0,0 +1,14 @@
+setInterval(() => {
+  getImage()
+},1000);
+function getImage(){
+ document.querySelector("#webpage").innerHTML = `<img src="https://images4.fanpop.com/image/photos/16100000/Cute-Kitten-kittens-16122061-1280-800.jpg" alt=""/>`
+}
+
+document.querySelector(".btn.btn-secondary").addEventListener("click", function(){
+  document.querySelector(".wrapper").style.display = "none"
+  setInterval(() => {
+    document.querySelector(".wrapper").style.display = "block"
+    document.querySelector("#loading").style.display = "none"
+   },20000)
+})
